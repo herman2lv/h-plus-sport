6a249d13a720139a0441e5256e1cac72f83d6542
Herman2lv <Herman2lv@gmail.com>
2021-02-18 17:16:27 +0300

    Change home servlet url patterns

--- src/com/epam/hplus/servlets/HomeServlet.java
+++ src/com/epam/hplus/servlets/HomeServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/home", "/"})
+@WebServlet(urlPatterns = {"/home", "/index.html"})
 public class HomeServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
