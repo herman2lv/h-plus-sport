dc16c0e812683bfa7980a109d46e30011b15407b
Herman2lv <Herman2lv@gmail.com>
2021-03-03 13:16:28 +0300

    Add MessageManager tests

--- src/test/java/com/epam/hplus/util/resources/MessageManagerTest.java
+++ src/test/java/com/epam/hplus/util/resources/MessageManagerTest.java
@@ -1,4 +1,49 @@
 package com.epam.hplus.util.resources;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Locale;
+
 public class MessageManagerTest {
+    private final static String KEY = "msg.unauthorized";
+    private final static String RU_MESSAGE =
+            "Вы не авторизованы. Пожалуйста, войдите под своим логином/зарегистрируйтесь";
+    private final static String EN_MESSAGE = "You are not authorized. Please, login";
+    private final static String RUSSIAN = "ru";
+    private final static String ENGLISH = "en";
+
+    @Test
+    public void getMessageDefaultLocaleTest() {
+        MessageManager.setLocale(Locale.getDefault().toString());
+        Assert.assertEquals(EN_MESSAGE, MessageManager.getMessage(KEY));
+    }
+
+    @Test
+    public void getMessageEnglishSystemLocaleTest() {
+        MessageManager.setLocale(Locale.ENGLISH.toString());
+        Assert.assertEquals(EN_MESSAGE, MessageManager.getMessage(KEY));
+    }
+
+    @Test
+    public void getMessageRussianLocaleTest() {
+        MessageManager.setLocale(RUSSIAN);
+        Assert.assertEquals(RU_MESSAGE, MessageManager.getMessage(KEY));
+    }
+
+    @Test
+    public void getMessageEnglishLocaleTest() {
+        MessageManager.setLocale(ENGLISH);
+        Assert.assertEquals(EN_MESSAGE, MessageManager.getMessage(KEY));
+    }
+
+    @Test
+    public void getMessageChangeLocaleTest() {
+        for (int i = 0; i < 3; i++) {
+            MessageManager.setLocale(ENGLISH);
+            Assert.assertEquals(EN_MESSAGE, MessageManager.getMessage(KEY));
+            MessageManager.setLocale(RUSSIAN);
+            Assert.assertEquals(RU_MESSAGE, MessageManager.getMessage(KEY));
+        }
+    }
 }
