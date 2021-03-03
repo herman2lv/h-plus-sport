b3adcbb2cc6d530765e3171f3747c5ca31ffdf87
Herman2lv <Herman2lv@gmail.com>
2021-03-03 17:50:08 +0300

    Add javadoc comments for Order bean class

--- src/main/java/com/epam/hplus/model/beans/Order.java
+++ src/main/java/com/epam/hplus/model/beans/Order.java
@@ -7,14 +7,52 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * Bean class of model layer represents user's order
+ */
 public class Order {
+    /**
+     * Unique identification numeric value to define the order
+     */
     private long orderId;
+    /**
+     * Name of the {@code User} which is owner of the order
+     */
     private String username;
+    /**
+     * Date of order creation
+     */
     private Date orderDate;
+    /**
+     * List of products represented by {@code Map} of {@code Product} and
+     * {@code Long} that represents product's number
+     */
     private Map<Product, Long> listOfProducts;
+    /**
+     * Total cost of the {@code Order}. The sum of price of all
+     * {@code Product} in the order
+     */
     private BigDecimal orderCost;
+    /**
+     * Confirmation status of the order. {@code True} stands for approved
+     * and {@code false} stands for rejected/pending
+     */
     private boolean status;
 
+    /**
+     * Constructor with all parameters, used to create instance of
+     * {@code Order} which already has unique {@param orderId} and
+     * to retrieve information about user's order from data storage
+     * @param orderId {@code long} value of unique order identification number
+     * @param username {@code String} represents name of order's owner
+     * @param orderDate {@code Date} of order creation
+     * @param listOfProducts {@code Map} represents list of orders
+     *                                  and its number
+     * @param orderCost {@code BigDecimal} value of total cost of order's
+     *                                    {@code Product}
+     * @param status {@code boolean} value represents confirmation status
+     *                              of teh order
+     */
     public Order(long orderId, String username, Date orderDate,
                  Map<Product, Long> listOfProducts, BigDecimal orderCost, boolean status) {
         this.orderId = orderId;
@@ -25,6 +63,19 @@ public class Order {
         this.status = status;
     }
 
+    /**
+     * Constructor with all parameters except {@param orderId}, used to create
+     * instance of {@code Order} which doesn't have unique {@param orderId}
+     * yet and to sen information about user's order to data storage
+     * @param username {@code String} represents name of order's owner
+     * @param orderDate {@code Date} of order creation
+     * @param listOfProducts {@code Map} represents list of orders and its
+     *                                  number
+     * @param orderCost {@code BigDecimal} value of total cost of order's
+     *                                    {@code Product}
+     * @param status {@code boolean} value represents confirmation status
+     *                              of teh order
+     */
     public Order(String username, Date orderDate,
                  Map<Product, Long> listOfProducts, BigDecimal orderCost, boolean status) {
         this.username = username;
@@ -34,50 +85,104 @@ public class Order {
         this.status = status;
     }
 
+    /**
+     * Standard getter method to access private class member
+     * @return {@code long} orderId
+     */
     public long getOrderId() {
         return orderId;
     }
 
+    /**
+     * Standard setter method to access private class member
+     * @param orderId {@code long} value of unique order identification number
+     */
     public void setOrderId(long orderId) {
         this.orderId = orderId;
     }
 
+    /**
+     * Standard getter method to access private class member
+     * @return {@code String} username represents name of order's owner
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Standard setter method to access private class member
+     * @param username {@code String} represents name of order's owner
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Standard getter method to access private class member
+     * @return {@code Date} of order creation
+     */
     public Date getOrderDate() {
         return orderDate;
     }
 
+    /**
+     * Standard setter method to access private class member
+     * @param orderDate {@code Date} of order creation
+     */
     public void setOrderDate(Date orderDate) {
         this.orderDate = orderDate;
     }
 
+    /**
+     * Getter method returns iterable version of list of {@code Product}
+     * in order to facilitate process of displaying and processing of data
+     * @return {@code List} of {@code Map.Entry} of {@code Product} and
+     * its number represented by {@code Long} value
+     */
     public List<Entry<Product, Long>> getListOfProducts() {
         return new ArrayList<>(listOfProducts.entrySet());
     }
 
+    /**
+     * Standard setter method to access private class member
+     * @param listOfProducts {@code Map} represents list of orders and its
+     *                                  number
+     */
     public void setListOfProducts(Map<Product, Long> listOfProducts) {
         this.listOfProducts = listOfProducts;
     }
 
+    /**
+     * Standard getter method to access private class member
+     * @return {@code BigDecimal} value of total cost of all order's products
+     */
     public BigDecimal getOrderCost() {
         return orderCost;
     }
 
+    /**
+     * Standard setter method to access private class member
+     * @param orderCost {@code BigDecimal} value of total cost of order's
+     *                                    {@code Product}
+     */
     public void setOrderCost(BigDecimal orderCost) {
         this.orderCost = orderCost;
     }
 
+    /**
+     * Standard getter method to access private class member
+     * @return {@code boolean} value that represents confirmation
+     * status of the order
+     */
     public boolean isStatus() {
         return status;
     }
 
+    /**
+     * Standard setter method to access private class member
+     * @param status {@code boolean} value represents confirmation status
+     *                              of teh order
+     */
     public void setStatus(boolean status) {
         this.status = status;
     }
